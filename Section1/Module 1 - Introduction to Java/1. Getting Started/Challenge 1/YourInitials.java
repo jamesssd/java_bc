7b0072eecd417dcee843901f7c52665cb585b836
James Dizon <dizonjerlin@gmail.com>
2022-08-02 22:53:00 -0700

    added bc folders

---
+++ Section1/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println("JJJJJ  DDDDD");
+        System.out.println("  J    D    D");
+        System.out.println("  J    D    D");
+        System.out.println("  J    D    D");
+        System.out.println("  J    D    D");
+        System.out.println("J J    D    D");
+        System.out.println(" JJ    DDDDD");
+
+    }
+}
