21972efee740c14874b670ad88f44cfda40aa4db
James Dizon <dizonjerlin@gmail.com>
2022-08-03 13:08:53 -0700

    went over int and long

--- Section2/2. Variables/Workbook 2.2/Biography.java
+++ Section2/2. Variables/Workbook 2.2/Biography.java
@@ -9,11 +9,19 @@ public class Biography {
         //make a game variable
         //make a subject variable
         //make a grade variable
+
+        String name = "James";
+        String country = "PI";
+        String sport = "None";
+        int hours = 0;
+        String game = "none";
+        String subject = "coding";
+        char grade = 'A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name + ". I'm <age> years old, and I'm from " + country);
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored a " + grade + ".");
 
     }
 }
