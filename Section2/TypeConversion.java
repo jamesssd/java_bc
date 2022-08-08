afff4162eafeb3f2e4aa583ee2bf46e9bbea99a2
James Dizon <dizonjerlin@gmail.com>
2022-08-08 11:45:58 -0700

    Completed workbook 2.4

---
+++ Section2/TypeConversion.java
@@ -0,0 +1,11 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        double salary = 5833.333333;
+        int roundedSalary = (int)salary;
+        System.out.println("Percy's salary is $ " + salary);
+        System.out.println("Percy's salary rounded $ " + roundedSalary);
+
+        
+        
+    }
+}
