137d163abad56becb738613e7dc93d300004a4f5
James Dizon <dizonjerlin@gmail.com>
2022-08-08 14:45:57 -0700

    Complete WB 3.2

--- Section3/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Section3/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -4,7 +4,13 @@ public class JavaMart {
         
         double toyCar = 5.99;
         System.out.println("Can I get this car?");
-       
+        
+        if(wallet > toyCar){
+            wallet = wallet - toyCar;
+            System.out.println("Sure!");
+        } else{
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
         //if you have enough money
             //       print: Sure!
             //       pay for the toy car 
@@ -14,6 +20,12 @@ public class JavaMart {
         
         double nike = 95.99;       //run test case with 89.99
         System.out.println("Can I get these nike shoes?");
+
+        if(wallet > nike){
+            System.out.println("Sure!");
+        } else{
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
         //if you have enough money
             //       print: Sure!
             //       pay for the nike shoes 
