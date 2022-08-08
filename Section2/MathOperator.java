513c6149a2b95385bf39957f8ad70995bf56ec14
James Dizon <dizonjerlin@gmail.com>
2022-08-08 11:35:36 -0700

    completed workbook 2.3

--- Section2/MathOperator.java
+++ Section2/MathOperator.java
@@ -23,7 +23,19 @@ public class MathOperator {
         System.out.println("Originally, Fred had " + (bagOfSweets2/bagOfSweets1) + " times more sweets than George");
         System.out.println("Andromeda has " + (starsInAndromeda/starsInMilkyWay) + " times more stars");
         System.out.println("Snape caught the error. Furious, he divided Ron's score to: " + (testScores/bonusMarks));
+
+        System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: "+ (10%2));
+        System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5%2));
+
+        int counter = 0;
+        counter++;
+        counter++;
+        counter++;
+        System.out.println("I counted: " + counter);
+        counter--;
+        counter--;
+        counter--;
+        System.out.println("I counted: " + counter);
     }
 
 }
-
