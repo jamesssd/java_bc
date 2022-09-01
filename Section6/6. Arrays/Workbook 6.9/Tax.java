0da68aeea64f1a55f47f8138cacb66baf9739d24
James Dizon <dizonjerlin@gmail.com>
2022-09-01 05:34:43 -0700

    Completed WB 6.9

--- Section6/6. Arrays/Workbook 6.9/Tax.java
+++ Section6/6. Arrays/Workbook 6.9/Tax.java
@@ -3,6 +3,16 @@ import java.util.Arrays;
 public class Tax {
     public static void main(String[] args) {
         double[] price = {1.99, 2.99, 3.99, 4.99};
+
+        double [] newPrice = new double[4];
+
+        newPrice[0] = price[0] * .13 + price[0];
+        newPrice[1] = price[1] * .13 + price[1];
+        newPrice[2] = price[2] * .13 + price[2];
+        newPrice[3] = price[3] * .13 + price[3];
+
+        System.out.println("The original prices are: " + Arrays.toString(price));
+        System.out.println("The prices after tax are: " + Arrays.toString(newPrice));
         /** Task 1: 
          * 
          *  Create a new array afterTax with the same length as price
