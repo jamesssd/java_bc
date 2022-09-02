1d5fe057718faed6cc95a8d204a4c33fbb48f1d9
James Dizon <dizonjerlin@gmail.com>
2022-09-02 00:43:10 -0700

    Started WB 6.10

--- Section6/6. Arrays/Workbook 6.10/PizzaDelivery.java
+++ Section6/6. Arrays/Workbook 6.10/PizzaDelivery.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 public class PizzaDelivery {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many pizza toppings do you want?");
+        int input = scan.nextInt();
+
         /**  Task 1: 
          *   1. Ask the user: How many pizza toppings do you want?. 
          *   2. Then, pick up the result using Scanner.
          */
-
+        String[] toppings = {};
         // Task 2 – Create the array here
-
+        System.out.println("Great! enter each topping!");
+        
+    
+        
+        for(int i = 0; i < toppings.length; i++){
+            String Input = scan.nextLine();
+            toppings[i] = Input;
+            System.out.println(i+ ". " + toppings[i] );
+        }
         /** Task 3
          *  print Great, enter each topping!
          *  Create a for loop that runs through the length of the array.   
