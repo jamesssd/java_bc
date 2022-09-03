822ae0f3a7049949cc7344fd56a477aaf3670bfa
James Dizon <dizonjerlin@gmail.com>
2022-09-02 20:05:11 -0700

    Completed WB 6.10

--- Section6/6. Arrays/Workbook 6.10/PizzaDelivery.java
+++ Section6/6. Arrays/Workbook 6.10/PizzaDelivery.java
@@ -5,28 +5,37 @@ public class PizzaDelivery {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("How many pizza toppings do you want?");
-        int input = scan.nextInt();
+        int input = scan.nextInt(); scan.nextLine();
 
         /**  Task 1: 
          *   1. Ask the user: How many pizza toppings do you want?. 
          *   2. Then, pick up the result using Scanner.
          */
-        String[] toppings = {};
+        String[] toppings = new String[input];
         // Task 2 – Create the array here
         System.out.println("Great! enter each topping!");
         
-    
-        
         for(int i = 0; i < toppings.length; i++){
+            System.out.print(i+ ". ");
             String Input = scan.nextLine();
             toppings[i] = Input;
-            System.out.println(i+ ". " + toppings[i] );
+            
         }
         /** Task 3
          *  print Great, enter each topping!
          *  Create a for loop that runs through the length of the array.   
          * 
          */
+        System.out.println("\nThank you! Here are the toppings your ordered");
+        for(int i = 0; i<toppings.length; i++){
+            System.out.println(i+". " + toppings[i]);
+        }
+
+        System.out.println("\nPress anything to confirm your order.");
+        scan.nextLine();
+        System.out.println("Great, a driver is on the way!");
+
+        scan.close();
 
         /** Task 4 – Pick up the user's toppings and store them in the array.
          *  
